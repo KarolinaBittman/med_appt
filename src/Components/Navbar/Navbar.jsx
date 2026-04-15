72fd561c8dffc2d21cf5df02b3abd2cda21f9850
KarolinaBittman <karolina.bittman@gmail.com>
2026-04-15 10:28:16 -0400

    remove svg from navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,13 +23,8 @@ function Navbar() {
         <div className="nav__logo">
           <a href="/">
             StayHealthy
-            <svg xmlns="http://www.w3.org/2000/svg" height="26" width="26" viewBox="0 0 1000 1000" style={{fill:"#3685fb"}}>
-              <title>Doctor With Stethoscope SVG icon</title>
-              <g><g>
-                <path d="M499.8,10c91.7,0,166,74.3,166,166c0,91.7-74.3,166-166,166c-91.7,0-166-74.3-166-166C333.8,84.3,408.1,10,499.8,10z"></path>
-                <path d="M499.8,522.8c71.2,0,129.1-58.7,129.1-129.1H370.6C370.6,464.1,428.6,522.8,499.8,522.8z"></path>
-                <path d="M693.2,395c-0.7,94.9-70.3,173.7-160.8,188.9v155.9c0,80.3-60.7,150.8-140.8,155.3c-83,4.7-152.7-58.9-157.6-139.7c-22-12.8-35.6-38.5-30.3-66.7c4.7-25.1,25.5-45.6,50.8-49.9c39.7-6.7,74.1,23.7,74.1,62.1c0,23-12.3,43-30.7,54.1c4.7,45.4,45.1,80.4,92.6,76c44.6-4,77.2-44...."></path>
-              </g></g>
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#3685fb" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
             </svg>
           </a>
           <span>.</span>
@@ -41,7 +36,7 @@ function Navbar() {
 
         <ul className={`nav__links ${isActive ? "active" : ""}`}>
           <li className="link"><a href="/">Home</a></li>
-          <li className="link"><a href="#">Appointments</a></li>
+          <li className="link"><a href="/appointment">Appointments</a></li>
           {sessionStorage.getItem("auth-token") ? (
             <>
               <li className="link">
